545c43f897544a5462135586e764a8bb8d51dfd7
Jannis Pohlmann <contact@jannispohlmann.de>
2019-01-23 17:35:48 -0800

    src: Add a first cut of the counter mapping

---
+++ src/mapping.ts
@@ -0,0 +1,11 @@
+import { Counter as Contract, Incremented } from './types/Counter/Counter'
+import { Counter } from './types/schema'
+
+export function handleIncremented(event: Incremented) {
+  let counter = Counter.load('default-counter')
+  if (counter == null) {
+    counter = new Counter('default-counter')
+  }
+  counter.value = event.params.value
+  counter.save()
+}
