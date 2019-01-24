b0389da029bddcfaab4f59743096401afef5ab13
Jannis Pohlmann <contact@jannispohlmann.de>
2019-01-23 18:50:10 -0800

    src, subgraph.yaml: Update mapping and add contract address to manifest

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,7 @@
 import { Counter as Contract, Incremented } from './types/Counter/Counter'
 import { Counter } from './types/schema'
 
-export function handleIncremented(event: Incremented) {
+export function handleIncremented(event: Incremented): void {
   let counter = Counter.load('default-counter')
   if (counter == null) {
     counter = new Counter('default-counter')
