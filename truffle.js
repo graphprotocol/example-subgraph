8b456faa931755e0a735cf6e13db258a47d51c8d
Nena <nena@thegraph.com>
2019-02-15 18:47:46 -0700

    updated example

---
+++ truffle.js
@@ -0,0 +1,22 @@
+require('babel-register')
+require('babel-polyfill')
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_API_KEY}`
+        )
+      },
+      network_id: '3',
+    },
+  },
+}
