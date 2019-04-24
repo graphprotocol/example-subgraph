9cd15e1abf762ac680e609b64a624ad51754e289
Jannis Pohlmann <jannis@thegraph.com>
2019-04-25 00:29:09 +0200

    WIP: Test block & call triggers

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,31 @@
-import { NewGravatar, UpdatedGravatar } from './types/Gravity/Gravity'
-import { Gravatar } from './types/schema'
+import { EthereumBlock } from "@graphprotocol/graph-ts";
+import {
+  NewGravatar,
+  UpdatedGravatar,
+  CreateGravatarCall
+} from "./types/Gravity/Gravity";
+import { Gravatar } from "./types/schema";
+
+export function handleNewBlock(block: EthereumBlock): void {}
+
+export function handleCreateGravatar(call: CreateGravatarCall): void {}
 
 export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  let gravatar = new Gravatar(event.params.id.toHex());
+  gravatar.owner = event.params.owner;
+  gravatar.displayName = event.params.displayName;
+  gravatar.imageUrl = event.params.imageUrl;
+  gravatar.save();
 }
 
 export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
+  let id = event.params.id.toHex();
+  let gravatar = Gravatar.load(id);
   if (gravatar == null) {
-    gravatar = new Gravatar(id)
+    gravatar = new Gravatar(id);
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  gravatar.owner = event.params.owner;
+  gravatar.displayName = event.params.displayName;
+  gravatar.imageUrl = event.params.imageUrl;
+  gravatar.save();
 }
