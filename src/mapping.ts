df1f99a87e2017f1976b81dffd051b4df3acccb3
azf20 <azfuller20@gmail.com>
2021-10-26 00:23:38 +0100

    near: simple blocks subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,34 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { near, BigInt } from "@graphprotocol/graph-ts";
+import { BlockEvent } from "../generated/schema";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+export function handleBlock(block: near.Block): void {
+  const header = block.header;
+  let event = new BlockEvent(header.hash.toHexString());
+  event.number = BigInt.fromI32(header.height as i32);
+  event.height = BigInt.fromI32(block.header.height as i32);
+  event.prevHeight = BigInt.fromI32(block.header.prevHeight as i32);
+  event.blockOrdinal = BigInt.fromI32(block.header.blockOrdinal as i32);
+  event.epochId = block.header.epochId;
+  event.nextEpochId = block.header.nextEpochId;
+  event.chunksIncluded = BigInt.fromI32(block.header.chunksIncluded as i32);
+  event.hash = block.header.hash;
+  event.prevHash = block.header.prevHash;
+  event.timestampNanosec = BigInt.fromI32(block.header.timestampNanosec as i32);
+  event.prevStateRoot = block.header.prevStateRoot;
+  event.chunkReceiptsRoot = block.header.chunkReceiptsRoot;
+  event.chunkHeadersRoot = block.header.chunkHeadersRoot;
+  event.chunkTxRoot = block.header.chunkTxRoot;
+  event.outcomeRoot = block.header.outcomeRoot;
+  event.challengesRoot = block.header.challengesRoot;
+  event.randomValue = block.header.randomValue;
+  event.gasPrice = block.header.gasPrice;
+  event.totalSupply = block.header.totalSupply;
+  event.lastFinalBlock = block.header.lastFinalBlock;
+  event.lastDsFinalBlock = block.header.lastDsFinalBlock;
+  event.nextBpHash = block.header.nextBpHash;
+  event.blockMerkleRoot = block.header.blockMerkleRoot;
+  event.epochSyncDataHash = block.header.epochSyncDataHash;
+  event.latestProtocolVersion = block.header.latestProtocolVersion;
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  event.save();
 }
