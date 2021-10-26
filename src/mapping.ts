e201b5d425b5c6436f4600a4f5587605c5977084
azf20 <azfuller20@gmail.com>
2021-10-26 01:27:29 +0100

    simplify

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,30 +5,8 @@ export function handleBlock(block: near.Block): void {
   const header = block.header;
   let event = new BlockEvent(header.hash.toHexString());
   event.number = BigInt.fromI32(header.height as i32);
-  event.height = BigInt.fromI32(block.header.height as i32);
-  event.prevHeight = BigInt.fromI32(block.header.prevHeight as i32);
-  event.blockOrdinal = BigInt.fromI32(block.header.blockOrdinal as i32);
-  event.epochId = block.header.epochId;
-  event.nextEpochId = block.header.nextEpochId;
-  event.chunksIncluded = BigInt.fromI32(block.header.chunksIncluded as i32);
   event.hash = block.header.hash;
-  event.prevHash = block.header.prevHash;
   event.timestampNanosec = BigInt.fromI32(block.header.timestampNanosec as i32);
-  event.prevStateRoot = block.header.prevStateRoot;
-  event.chunkReceiptsRoot = block.header.chunkReceiptsRoot;
-  event.chunkHeadersRoot = block.header.chunkHeadersRoot;
-  event.chunkTxRoot = block.header.chunkTxRoot;
-  event.outcomeRoot = block.header.outcomeRoot;
-  event.challengesRoot = block.header.challengesRoot;
-  event.randomValue = block.header.randomValue;
-  event.gasPrice = block.header.gasPrice;
-  event.totalSupply = block.header.totalSupply;
-  event.lastFinalBlock = block.header.lastFinalBlock;
-  event.lastDsFinalBlock = block.header.lastDsFinalBlock;
-  event.nextBpHash = block.header.nextBpHash;
-  event.blockMerkleRoot = block.header.blockMerkleRoot;
-  event.epochSyncDataHash = block.header.epochSyncDataHash;
-  event.latestProtocolVersion = block.header.latestProtocolVersion;
 
   event.save();
 }
