56b12af3fa049e752bc2ad5b434a1f5728ab2156
Marc Puig <krusspy@gmail.com>
2022-04-25 20:17:38 +0200

    Cosmos transaction subgraph example

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,31 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { tendermint, cosmos } from "@graphprotocol/graph-ts";
+import { Delegation, Coin } from "../generated/schema";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleTx(transactionData: tendermint.TransactionData): void {
+  const id = `${transactionData.block.blockId.hash.toHexString()}-${transactionData.tx.index}`;
+  const messages = transactionData.tx.tx.body.messages;
+
+  for (let i = 0; i < messages.length; i++) {
+    let msgType = messages[i].typeUrl;
+    let msgValue = messages[i].value as Uint8Array;
+    if (msgType == "/cosmos.staking.v1beta1.MsgDelegate") {
+      saveDelegation(id, cosmos.v1.decodeMsgDelegate(msgValue)) // The message needs to be decoded to access its attributes.
+    }
+  }
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+function saveDelegation(id: string, message: cosmos.v1.MsgDelegate): void {
+  const msg = new Delegation(id);
+  msg.delegatorAddress = message.delegator_address;
+  msg.validatorAddress = message.validator_address;
+  msg.amount = saveCoin(id, message.amount as cosmos.v1.Coin);
+  msg.save();
+}
+
+function saveCoin(id: string, c: cosmos.v1.Coin): string {
+  const coin = new Coin(id);
+  coin.amount = c.amount;
+  coin.denom = c.denom;
+  coin.save();
+  return id;
 }
