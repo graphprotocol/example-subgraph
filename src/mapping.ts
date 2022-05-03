5282d3b68607793af3e3dee281e43aeaa3286d95
Marc Puig <krusspy@gmail.com>
2022-05-03 19:38:05 +0200

    Renaming

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,9 @@
-import { BigInt, tendermint } from "@graphprotocol/graph-ts";
+import { BigInt, cosmos } from "@graphprotocol/graph-ts";
 import { Block } from "../generated/schema";
 
-export function handleBlock(el: tendermint.EventList): void {
-  const header = el.newBlock.block.header;
-  const hash = el.newBlock.blockId.hash.toHexString();
+export function handleBlock(bl: cosmos.Block): void {
+  const header = bl.header;
+  const hash = bl.header.hash.toHexString();
   const height = BigInt.fromString(header.height.toString());
 
   const block = new Block(hash);
