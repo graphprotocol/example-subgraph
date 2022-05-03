465d99951e414029be69e39facd37435edfda07f
Marc Puig <krusspy@gmail.com>
2022-05-03 19:49:45 +0200

    Renaming

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,8 @@
-import { tendermint } from "@graphprotocol/graph-ts";
+import { cosmos } from "@graphprotocol/graph-ts";
 import { Reward } from "../generated/schema";
 
-export function handleReward(eventData: tendermint.EventData): void {
-  const height = eventData.block.block.header.height;
+export function handleReward(eventData: cosmos.EventData): void {
+  const height = eventData.block.header.height;
   const amount = eventData.event.attributes[0].value;
   const validator = eventData.event.attributes[1].value;
 
