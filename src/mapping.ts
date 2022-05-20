7e1d79466d423583c19215befd7f69dc05e30f88
Bartosz Pieńkowski <pienkowb@gmail.com>
2022-05-20 15:59:21 +0200

    Use the official Graph packages

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,13 +2,14 @@ import { cosmos } from "@graphprotocol/graph-ts";
 import { MsgDelegate, MsgCoin, decodeMsgDelegate } from "./decoding";
 import { Delegation, Coin } from "../generated/schema";
 
-export function handleTx(transactionData: cosmos.TransactionData): void {
-  const id = `${transactionData.block.header.hash.toHexString()}-${transactionData.tx.index}`;
-  const messages = transactionData.tx.tx.body.messages;
+export function handleTx(data: cosmos.TransactionData): void {
+  const id = `${data.block.header.hash.toHexString()}-${data.tx.index}`;
+  const messages = data.tx.tx.body.messages;
 
   for (let i = 0; i < messages.length; i++) {
     let msgType = messages[i].typeUrl;
     let msgValue = messages[i].value as Uint8Array;
+
     if (msgType == "/cosmos.staking.v1beta1.MsgDelegate") {
       saveDelegation(id, decodeMsgDelegate(msgValue)) // The message needs to be decoded to access its attributes.
     }
@@ -17,16 +18,21 @@ export function handleTx(transactionData: cosmos.TransactionData): void {
 
 function saveDelegation(id: string, message: MsgDelegate): void {
   const msg = new Delegation(id);
+
   msg.delegatorAddress = message.delegator_address;
   msg.validatorAddress = message.validator_address;
   msg.amount = saveCoin(id, message.amount as MsgCoin);
+
   msg.save();
 }
 
 function saveCoin(id: string, c: MsgCoin): string {
   const coin = new Coin(id);
+
   coin.amount = c.amount;
   coin.denom = c.denom;
+
   coin.save();
+
   return id;
 }
