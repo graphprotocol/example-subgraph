e8c0c23f491ffcd706ed1491a7b6f7bc2274e614
Seb-figment <sebastien.nobert@figment.io>
2022-06-27 17:06:55 -0400

    cosmwasm-example

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,35 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { BigInt, cosmos, log } from "@graphprotocol/graph-ts";
+import { cosmwasm } from "@graphprotocol/juno-ts";
+import { Contract } from "../generated/schema";
+import { JSON } from "assemblyscript-json";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+export function handleMsgExecuteContract(messageData: cosmos.MessageData): void {
+  const message = cosmwasm.wasm.v1.decodeMsgExecuteContract(messageData.message.value);
+  const id = message.contract;
+  let contract = Contract.load(id);
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+  if (contract == null) {
+    contract  = new Contract(id);
+    contract.liquidity = BigInt.fromI32(0);
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+  const jsonMsg = <JSON.Obj>(JSON.parse(message.msg));
+
+  const add_liquidity = jsonMsg.getObj("add_liquidity");
+  if (add_liquidity != null) {
+    const amount = add_liquidity.getString("token1_amount");
+    if (amount != null) {
+      contract.liquidity = contract.liquidity.plus(BigInt.fromString(amount.toString()));
+    }
+  }
+
+  const remove_liquidity = jsonMsg.getObj("remove_liquidity");
+  if (remove_liquidity != null) {
+    const amount = remove_liquidity.getString("amount");
+    if (amount != null) {
+      contract.liquidity = contract.liquidity.minus(BigInt.fromString(amount.toString()));
+    }
+  }
+
+  contract.save();
 }
